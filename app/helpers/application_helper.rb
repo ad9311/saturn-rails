7d3e51336776e2decf3e3a239c1cfbb6e8b53f86
Ángel Díaz <diazangel9311@gmail.com>
2023-06-06 23:52:38 -0500

    Implement show recovery styles
    Improve show submit report button

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,12 @@ module ApplicationHelper
   def recovery_target_days(recovery)
     (recovery.target_date - recovery.start_date).to_i
   end
+
+  def recovery_last_report_today?(recovery)
+    return false if recovery.report_dates.last.nil?
+
+    return true if Date.parse(recovery.report_dates.last) == Time.zone.now.to_date
+
+    false
+  end
 end
