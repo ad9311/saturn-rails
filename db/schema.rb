06d419c7096c21b38ef804789ac587ccadaa2719
Ángel Díaz <diazangel9311@gmail.com>
2023-06-07 21:59:45 -0500

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_07_171636) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_08_020836) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -41,6 +41,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_07_171636) do
     t.index ["user_id"], name: "index_recoveries_on_user_id"
   end
 
+  create_table "stoppers", force: :cascade do |t|
+    t.datetime "datetime", null: false
+    t.text "reason", null: false
+    t.bigint "recovery_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["recovery_id"], name: "index_stoppers_on_recovery_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "first_name", null: false
     t.string "last_name", null: false
@@ -61,4 +70,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_07_171636) do
 
   add_foreign_key "awards", "users"
   add_foreign_key "recoveries", "users"
+  add_foreign_key "stoppers", "recoveries"
 end
