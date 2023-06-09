4a9a886f7bfe7b91ca91c1d7050cb1f23f2a7a17
Ángel Díaz <diazangel9311@gmail.com>
2023-06-08 21:07:17 -0500

    Implement thoughts controller and view
    Make navbar sticky top
    Add padding to main element tag
    Remove properties to html

---
+++ app/controllers/thoughts_controller.rb
@@ -0,0 +1,36 @@
+class ThoughtsController < ApplicationController
+  before_action :thought_params, only: %i[create]
+  before_action :set_thought, only: %i[destroy]
+
+  def index
+    @thoughts = current_user.thoughts
+  end
+
+  def new
+    @thought = Thought.new
+  end
+
+  def create
+    @thought = current_user.thoughts.build(thought_params)
+    if @thought.save
+      redirect_to thoughts_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @thought.destroy
+    redirect_to thoughts_path
+  end
+  
+  private
+
+  def thought_params
+    params.require(:thought).permit(:message, :mood)
+  end
+
+  def set_thought
+    @thought = Thought.find(params[:id])
+  end
+end
