a7e83f2fa0fbc4ea59cdf2e002d84a00fbf2b348
Ángel Díaz <diazangel9311@gmail.com>
2023-06-19 18:37:01 -0500

    Fix faulty migration

--- db/migrate/20230619232956_rename_budget_top_amount_allowed_to_amount.rb
+++ db/migrate/20230619232956_rename_budget_top_amount_allowed_to_amount.rb
@@ -1,5 +1,5 @@
 class RenameBudgetTopAmountAllowedToAmount < ActiveRecord::Migration[7.0]
   def change
-    rename_column :budgets, :amount, :amount
+    rename_column :budgets, :top_amount_allowed, :amount
   end
 end
