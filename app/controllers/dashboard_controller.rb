79e46d64e1ab9700107e7b40c4d137c6ed28f118
Ángel Díaz <diazangel9311@gmail.com>
2023-06-18 21:20:57 -0500

    Implement random reminder in Dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,6 @@
 class DashboardController < ApplicationController
   def index
+    @random_reminder = current_user.reminders.order('RANDOM()').first
     @reminders_count = current_user.reminders.count
     @thoughts_count = current_user.thoughts.count
     @recoveries_count = current_user.recoveries.count
